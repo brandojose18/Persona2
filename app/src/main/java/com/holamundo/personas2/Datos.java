c5d57960fc0698032633edfe9bb280db466f8c28
Brando <brandojose18@gmail.com>
2018-04-09 21:06:00 -0500

    Aplicacion Terminada sin validaciones

---
+++ app/src/main/java/com/holamundo/personas2/Datos.java
@@ -0,0 +1,20 @@
+package com.holamundo.personas2;
+
+import java.util.ArrayList;
+
+/**
+ * Created by android on 09/04/2018.
+ */
+
+public class Datos {
+    private static ArrayList<Persona> personas = new ArrayList<>();
+
+    public static void guardar(Persona p){
+        personas.add(p);
+    }
+
+    public static ArrayList<Persona> obtener(){
+        return personas;
+    }
+
+}
